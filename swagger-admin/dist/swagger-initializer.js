9044f4c7d24d0a5c9a9a5b9d4ae676733c5e5c16
Maryam <maryam.mehrabian@bouvet.no>
2025-05-21 15:32:28 +0200

    Added necessary files for swagger-admin

---
+++ swagger-admin/dist/swagger-initializer.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "swagger.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout",
+    supportedSubmitMethods: [],
+    // Hiding 'servers'-dropdown. Also hiding sub-objects from 'Scehmas'-list
+    onComplete: function() {
+        Array.from(document.getElementsByClassName("scheme-container")).forEach(
+            function (element, index, array) {
+              element.outerHTML = ""
+            })
+      document.getElementById("model-DatexApplicationApi").outerHTML = ""
+      document.getElementById("model-DenmApplicationApi").outerHTML = ""
+      document.getElementById("model-IvimApplicationApi").outerHTML = ""
+      document.getElementById("model-MapemApplicationApi").outerHTML = ""
+      document.getElementById("model-MetadataApi").outerHTML = ""
+      document.getElementById("model-SpatemApplicationApi").outerHTML = ""
+      document.getElementById("model-SremApplicationApi").outerHTML= ""
+      document.getElementById("model-SsemApplicationApi").outerHTML = ""
+    }
+  });
+  //</editor-fold>
+};
+
